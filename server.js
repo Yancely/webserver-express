788cff8f742693575e0952b89c3db1d1b25f6c3e
Veronica <elyrua122@gmail.com>
2020-02-14 11:13:58 -0500

    Primer comit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers')
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+ 
+// EXPRESS HBS engine
+
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+   app.get('/', (req, res) =>{
+   res.render('home', {
+       nombre : 'Veronica'
+   });
+
+});
+    app.get('/about', (req, res) =>{
+        res.render('about');
+    });
+
+
+
+// });
+//     let salida = {
+//         nombre:'fernando',
+//         edad: 32,
+//         url: req.url
+//     };
+
+//     res.send(salida);}
+// });
+
+// app.get('/data', (req, res) => { 
+//     res.send('Hola Data');
+
+app.listen(port, () => {
+    console.log('Escuchando peticiones en el puerto 3000');   
+});
